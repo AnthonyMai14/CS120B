0e972ea99969754637da8e024a97eb2f3a886d8e
fmuno003 <fmuno003@ucr.edu>
2016-02-22 17:45:20 -0800

    Added files via upload

---
+++ Lab1/fmuno003_lab1_part2.c
@@ -0,0 +1,37 @@
+/*
+ * fmuno003_lab1_part2.c
+ *
+ * Created: 1/6/2016 6:10:32 PM
+ *  Author: munoz
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char cntavail = 0x00;
+	unsigned char firstPark = 0x00;
+	unsigned char secondPark = 0x00;
+	unsigned char thirdPark = 0x00;
+	unsigned char fourthPark = 0x00;
+	
+    while(1)
+    {
+		firstPark = PINA & 0x01;
+		secondPark = PINA & 0x02;
+		thirdPark = PINA & 0x04;
+		fourthPark = PINA & 0x08;
+		
+		secondPark = secondPark >> 1;
+		thirdPark = thirdPark >> 2;
+		fourthPark = fourthPark >> 3;
+		
+		cntavail = firstPark + secondPark + thirdPark + fourthPark;	
+		cntavail = 0x04 - cntavail;	
+		PORTC = cntavail;
+    }
+}
---
+++ Lab1/fmuno003_lab1_part9.c
@@ -0,0 +1,38 @@
+/*
+ * fmuno003_lab1_part9.c
+ *
+ * Created: 1/6/2016 10:47:45 PM
+ *  Author: munoz
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRD = 0x00; PORTD = 0xFF;
+	DDRB = 0xFE; PORTB = 0x01;
+	
+	unsigned short sensor = 0x0000;
+	unsigned char temp = 0x00;
+	unsigned char output = 0x01;
+    while(1)
+    {
+        sensor = (PIND << 1);
+		temp = PINB & 0x01;
+		
+		sensor = sensor | temp;
+		
+		if(sensor >= 0x0046)
+		{
+			output = 0x02;
+		}			 
+		else if((sensor < 0x0046) && (sensor > 0x0005))
+		{
+			output = 0x04;
+		}
+		PORTB = output;
+		output = 0x01;
+		sensor = 0x0000;
+    }
+}
